06a3a7db9418bac2402ad80eb6f8a2b5d043da84
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2025-01-29 22:14:45 +0530

    clang format

--- Sortalgo/bubble.cpp
+++ Sortalgo/bubble.cpp
@@ -1,36 +1,28 @@
 #include <iostream>
 using namespace std;
-void s(int arr[], int n)
-{
-    for (int i = n - 1; i >= 0; i--)
-    {
+void s(int arr[], int n) {
+    for (int i = n - 1; i >= 0; i--) {
         int didSwap = 0;
-        for (int j = 0; j <= i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
+        for (int j = 0; j <= i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
                 didSwap = 1;
             }
         }
-        if (didSwap == 0)
-        {
+        if (didSwap == 0) {
             break;
         }
     }
 }
-int main()
-{
+int main() {
     int n;
     cin >> n;
     int arr[n];
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
     s(arr, n);
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
 
