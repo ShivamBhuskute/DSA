03025ef22a42fe58e7df2d65e024245219a33e28
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2025-02-05 21:03:08 +0530

    recursion

--- recursion/1ton.cpp
+++ recursion/1ton.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 using namespace std;
-void f(int i, int n)
-{
-    if (i > n)
-    {
+void f(int i, int n) {
+    if (i > n) {
         return;
     }
     cout << i << "\n";
     f(i + 1, n);
 }
 
-int main()
-{
+int main() {
     int n;
     cin >> n;
     f(1, n);
