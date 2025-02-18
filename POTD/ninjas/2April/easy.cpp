2062bd3e8f62a9764ac2e6fe17de622740e0ba95
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2025-02-18 22:04:16 +0530

    formatted

--- POTD/ninjas/2April/easy.cpp
+++ POTD/ninjas/2April/easy.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <climits>
-#include <string>
 #include <algorithm>
+#include <climits>
 #include <cmath>
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
-string findDivision(int x, int y, int n)
-{
+string findDivision(int x, int y, int n) {
     // Write your code here.
     string res;
-    if ((x < 0 || y < 0) && (x > 0 || y > 0))
-    {
+    if ((x < 0 || y < 0) && (x > 0 || y > 0)) {
         res += '-';
     }
     res += to_string(abs(x) / abs(y));
     res += '.';
     int rem = abs(x) % abs(y);
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         rem = rem * 10;
         int d = rem / abs(y);
         rem = rem % abs(y);
