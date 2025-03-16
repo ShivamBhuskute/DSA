49c0a31079ffdcb356470a694cc3e5592c1468e3
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2025-03-16 13:03:26 +0530

    indentation added

--- Array/hard/Pascal's triangle/pascaltri.cpp
+++ Array/hard/Pascal's triangle/pascaltri.cpp
@@ -1,33 +1,28 @@
+#include <climits>
 #include <iostream>
 #include <vector>
-#include <climits>
 using namespace std;
 
-int nCr(int n, int r) 
-{
+int nCr(int n, int r) {
     long long res = 1;
 
     // calculating nCr:
-    for (int i = 0; i < r; i++)
-    {
+    for (int i = 0; i < r; i++) {
         res = res * (n - i);
         res = res / (i + 1);
     }
     return res;
 }
 
-int pascalTriangle(int r, int c)
-{
+int pascalTriangle(int r, int c) {
     int element = nCr(r - 1, c - 1);
     return element;
 }
 
-int main()
-{
-    int r = 5; // row number
-    int c = 3; // col number
+int main() {
+    int r = 5;  // row number
+    int c = 3;  // col number
     int element = nCr(r - 1, c - 1);
-    cout << "The element at position (r,c) is: "
-         << element << "\n";
+    cout << "The element at position (r,c) is: " << element << "\n";
     return 0;
 }
